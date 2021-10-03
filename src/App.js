a030c905274099d15cd9b2ef43d5aa45ff712743
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-03 14:08:12 +0600

    update project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import User from './components/User/User';
+import Users from './components/Users/Users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/users">
+            <Users></Users>
+          </Route>
+          <Route path="/user">
+            <User></User>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route>
+            <PageNotFound></PageNotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
